fe4251cdef95cdc0824d1e2dda102fe1c9ca270d
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-07 14:07:37 +0300

    done

---
+++ migrations/versions/785cc47565b8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 785cc47565b8
+Revises: 
+Create Date: 2023-12-07 13:59:53.698393
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '785cc47565b8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
